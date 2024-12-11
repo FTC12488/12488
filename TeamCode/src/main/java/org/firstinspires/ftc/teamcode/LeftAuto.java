4c10cf2e8d8ea91f2ea4fbf0ecd00dba2cfb23d9
Ilost333times <andyl100774@gmail.com>
2024-12-11 15:42:43 -0500

    Right-Auton Drafted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftAuto.java
@@ -6,7 +6,6 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.robotParts.Claw;
 import org.firstinspires.ftc.teamcode.robotParts.DriveTrain;
 import org.firstinspires.ftc.teamcode.robotParts.LinearLift;
 import org.firstinspires.ftc.teamcode.robotParts.Turn;
@@ -17,51 +16,30 @@ public class LeftAuto extends LinearOpMode {
     TelemetryPacket packet = new TelemetryPacket();
     FtcDashboard dashboard = FtcDashboard.getInstance();
 
-    private final double[] PidConstantsAngle = new double[]{1, 200, 0};
+    public static double[] PidConstantsAngle = new double[]{1, 200, 0};
     private final double[] PidConstantsDistance = new double[]{0.0005, 0.01, 0};
 
     private final DriveTrain dt = new DriveTrain();
     private final LinearLift lin = new LinearLift();
     private final Turn turn = new Turn();
-    private final Claw claw = new Claw();
+//    private final Claw claw = new Claw();
 
     public static String[][] instructions = {
-            //Move forward
-            {"Drive", "0", "9000"},
-
-            //Align
+            //park lol
+            {"Drive", "270", "40000", "True"},
             {"Turn", "180"},
-            {"Rotate", "0.85"},
-
-            //Clip on
-            {"Drive", "0", "0"},
-            {"Rotate", "0.9"},
-            {"Drive", "0", "0"},
-            {"Claw", "Open"},
-
-            //Align to first sample
-            {"Rotate", "0.45"},
-            {"Turn", "180"},
-            {"Drive", "0", "0"}, //Diagonally Left
-            {"Drive", "0", "0"}, //Up
-
-            // Pickup
-            {"Claw", "Open"},
-            {"Rotate", "0.45"},
-            {"DLift", ".9"},
-            {"Claw", "Close"},
-            {"DLift", "0"},
-
-            // Place
-            {"Drive", "0", "0"},
-            {"Turn", "15"},
-            {"Lift", "3000"},
-            {"Rotate", "0.9"},
-            {"Claw", "Open"},
-            {"Rotate", "0.45"},
+            {"Lift", "1600"},
+            {"Drive", "180", "1000", "False"},
             {"Lift", "0"},
-
-
+            {"Drive", "0", "1000", "False"},
+            {"Turn", "180"},
+//            {"Drive", "135", "10000", "True"},
+//
+//            //Place
+//            {"Lift", "400"},
+//
+//            //Back up
+//            {"Drive", "180", "2000"},\
     };
 
     @Override
@@ -69,45 +47,71 @@ public class LeftAuto extends LinearOpMode {
         dt.init(hardwareMap);
         lin.init(hardwareMap);
         turn.init(hardwareMap);
-        claw.init(hardwareMap);
+//        claw.init(hardwareMap);
 
         waitForStart();
 
         for(String[] instruction : instructions){
             double theta = 0;
             double distance = 0;
+            double timeout = System.nanoTime();
             switch (instruction[0]) {
-                case "Drive":
+                case "Drive": //PARAM: 1 - Angle, 2 - Distance, 3 - Fix Rotation (Only use when driving with 0 starting orientation)
+                    double oldAngle = dt.getImu().getAngularOrientation().firstAngle;
                     theta = Double.parseDouble(instruction[1]);
                     distance = Double.parseDouble(instruction[2]);
                     dt.driveToLocation(PidConstantsDistance, theta, distance);
+
+                    //Fix Angle
+                    if(instruction[3].equals("True")) {
+                        timeout = System.nanoTime(); //Make sure it's basing the timeout on time passed since robot reached target loc
+                        theta = dt.getImu().getAngularOrientation().firstAngle-oldAngle;
+                        while(Math.abs((Math.toRadians(theta)-dt.getImu().getAngularOrientation().firstAngle)) > .02 && opModeIsActive()){
+                            if((System.nanoTime() - timeout)/1e9 > 1){
+                                break;
+                            }
+                            dt.fixAngle(PidConstantsAngle, theta);
+                            packet.put("Angle", dt.getImu().getAngularOrientation().firstAngle);
+                            dashboard.sendTelemetryPacket(packet);
+                        }
+                    }
+                    dt.zeroMotors();
                     break;
                 case "Turn":
                     theta = Double.parseDouble(instruction[1]);
-                    while(dt.getImu().getAngularOrientation().firstAngle > .05 && opModeIsActive()){
+                    while(Math.abs((Math.toRadians(theta)-dt.getImu().getAngularOrientation().firstAngle)) > .001 && opModeIsActive()){
+                        if((System.nanoTime() - timeout)/1e9 > 3){
+                            break;
+                        }
                         dt.fixAngle(PidConstantsAngle, theta);
-                        sleep(20);
+                        packet.put("Angle", dt.getImu().getAngularOrientation().firstAngle);
+                        dashboard.sendTelemetryPacket(packet);
                     }
+                    dt.zeroMotors();
                     break;
                 case "Lift":
                     distance = Double.parseDouble(instruction[1]);
-//                    lin.gotoPosition(distance);
+                    while(Math.abs(distance-lin.getPos()) > 45){
+                        lin.gotoPosition(distance, new CustomPID(PidConstantsAngle));
+                        packet.put("Lin", lin.getPos());
+                        dashboard.sendTelemetryPacket(packet);
+                    }
                     break;
                 case "DLift":
                     theta = Double.parseDouble(instruction[1]);
                     turn.gotoMaxPosition(theta);
                     break;
-                case "Rotate":
-                    theta = Double.parseDouble(instruction[1]);
+//                case "Rotate":
+//                    theta = Double.parseDouble(instruction[1]);
 //                    claw.setRotate(theta);
-                    break;
-                case "Claw":
-                    if (instruction[1].equals("Close")){
+//                    break;
+//                case "Claw":
+//                    if (instruction[1].equals("Close")){
 //                        claw.setClaw(.8);
-                    }else{
+//                    }else{
 //                        claw.setClaw(.5);
-                    }
-                    break;
+//                    }
+//                    break;
             }
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAuto.java
@@ -6,7 +6,6 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.robotParts.Claw;
 import org.firstinspires.ftc.teamcode.robotParts.DriveTrain;
 import org.firstinspires.ftc.teamcode.robotParts.LinearLift;
 import org.firstinspires.ftc.teamcode.robotParts.Turn;
@@ -17,27 +16,40 @@ public class RightAuto extends LinearOpMode {
     TelemetryPacket packet = new TelemetryPacket();
     FtcDashboard dashboard = FtcDashboard.getInstance();
 
-    private final double[] PidConstantsAngle = new double[]{1, 200, 0};
+    public static double[] PidConstantsAngle = new double[]{1, 200, 0};
     private final double[] PidConstantsDistance = new double[]{0.0005, 0.01, 0};
 
     private final DriveTrain dt = new DriveTrain();
     private final LinearLift lin = new LinearLift();
     private final Turn turn = new Turn();
-    private final Claw claw = new Claw();
+//    private final Claw claw = new Claw();
 
     public static String[][] instructions = {
-            //Figure n
-            {"Drive", "0", "9000"},
-            {"Drive", "270", "1000"},
-            {"Drive", "180", "9000"},
-            //Figure n
-            {"Drive", "0", "9000"},
-            {"Drive", "270", "1000"},
-            {"Drive", "180", "9000"},
-            //Figure n
-            {"Drive", "0", "9000"},
-            {"Drive", "270", "1000"},
-            {"Drive", "180", "9000"},
+            //park lol
+            {"Drive", "0", "8000", "True"},
+            {"Turn", "180"},
+//            {"DLift", "0"},
+            {"Lift", "1600"},
+            {"Drive", "180", "2000", "False"},
+            {"Lift", "0"},
+            {"Drive", "0", "2000", "False"},
+            {"Turn", "180"},
+            {"Drive", "180", "7500", "True"},
+
+            //Test Right Auton (Figure NNN - "Pushbot")
+//            {"Drive", "270", "8000", "True"},
+//            {"Drive", "0", "17000", "True"},
+//
+//            {"Drive", "270", "2000", "True"},
+//            {"Drive", "180", "14000", "True"},
+//            {"Drive", "0", "14000", "True"},
+//
+//            {"Drive", "270", "2000", "True"},
+//            {"Drive", "180", "14000", "True"},
+//            {"Drive", "0", "14000", "True"},
+//
+//            {"Drive", "270", "1000", "True"},
+//            {"Drive", "180", "15000", "True"},
     };
 
     @Override
@@ -45,42 +57,72 @@ public class RightAuto extends LinearOpMode {
         dt.init(hardwareMap);
         lin.init(hardwareMap);
         turn.init(hardwareMap);
-        claw.init(hardwareMap);
+//        claw.init(hardwareMap);
 
         waitForStart();
 
         for(String[] instruction : instructions){
             double theta = 0;
             double distance = 0;
+            double timeout = System.nanoTime();
             switch (instruction[0]) {
-                case "Drive":
+                case "Drive": //PARAM: 1 - Angle, 2 - Distance, 3 - Fix Rotation (Only use when driving with 0 starting orientation)
+                    double oldAngle = dt.getImu().getAngularOrientation().firstAngle;
                     theta = Double.parseDouble(instruction[1]);
                     distance = Double.parseDouble(instruction[2]);
                     dt.driveToLocation(PidConstantsDistance, theta, distance);
+
+                    //Fix Angle
+                    if(instruction[3].equals("True")) {
+                        timeout = System.nanoTime(); //Make sure it's basing the timeout on time passed since robot reached target loc
+                        theta = dt.getImu().getAngularOrientation().firstAngle-oldAngle;
+                        while(Math.abs((Math.toRadians(theta)-dt.getImu().getAngularOrientation().firstAngle)) > .02 && opModeIsActive()){
+                            if((System.nanoTime() - timeout)/1e9 > 1){
+                                break;
+                            }
+                            dt.fixAngle(PidConstantsAngle, theta);
+                            packet.put("Angle", dt.getImu().getAngularOrientation().firstAngle);
+                            dashboard.sendTelemetryPacket(packet);
+                        }
+                    }
+                    dt.zeroMotors();
                     break;
                 case "Turn":
                     theta = Double.parseDouble(instruction[1]);
-                    dt.fixAngle(PidConstantsAngle, theta);
+                    while(Math.abs((Math.toRadians(theta)-dt.getImu().getAngularOrientation().firstAngle)) > .001 && opModeIsActive()){
+                        if((System.nanoTime() - timeout)/1e9 > 3){
+                            break;
+                        }
+                        dt.fixAngle(PidConstantsAngle, theta);
+                        packet.put("Angle", dt.getImu().getAngularOrientation().firstAngle);
+                        dashboard.sendTelemetryPacket(packet);
+                    }
+                    dt.zeroMotors();
                     break;
                 case "Lift":
                     distance = Double.parseDouble(instruction[1]);
-//                    lin.gotoPosition(distance);
+                    while(Math.abs(distance-lin.getPos()) > 45){
+                        lin.gotoPosition(distance, new CustomPID(PidConstantsAngle));
+                        packet.put("Lin", lin.getPos());
+                        dashboard.sendTelemetryPacket(packet);
+                    }
+                    lin.setPower(0);
                     break;
                 case "DLift":
                     theta = Double.parseDouble(instruction[1]);
                     turn.gotoMaxPosition(theta);
                     break;
-                case "Rotate":
-                    theta = Double.parseDouble(instruction[1]);
+//                case "Rotate":
+//                    theta = Double.parseDouble(instruction[1]);
 //                    claw.setRotate(theta);
-                    break;
-                case "Claw":
-                    if (instruction[1].equals("Close")){
+//                    break;
+//                case "Claw":
+//                    if (instruction[1].equals("Close")){
 //                        claw.setClaw(.8);
-                    }else{
+//                    }else{
 //                        claw.setClaw(.5);
-                    }
-                    break;
+//                    }
+//                    break;
             }
         }
     }
